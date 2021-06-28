3337c4f08e236a6e7190a37093ce03405952c816
Keil <Keil.Schmit@tlgcohort.com>
2021-06-28 22:10:12 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#make list
+my_list=["192.168.0.5", 5060, "UP"]
+
+
+#print item one from list
+print("The first item in the list (IP): " + my_list[0])
+#print item two from list
+print("The second item in the list (port): " + str(my_list[1]))
+#print item three from list
+print("The last item in the list (state): " + my_list[2])
+
+
+
+iplist= [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+print("IPs are as follows:" , iplist[3], iplist[4])
+
