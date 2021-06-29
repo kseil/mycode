f934b53123901aa3ca3d161051508219c0cc2e7c
Keil <Keil.Schmit@tlgcohort.com>
2021-06-29 21:52:33 +0000

    join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#create smol string
+
+lilstring = "Atla3 Research offers classes on Python coding"
+newlist = lilstring.split(" ") #this returns a list
+print(newlist)
+
+#create a list of strings
+myiplist = ['192', '168', '0', '12']
+singleip = ".".join(myiplist)
+print(singleip)
+
