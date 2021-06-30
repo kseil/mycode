2dfe7a26402e6c9137ee5455e99d0141648d08d1
Keil <Keil.Schmit@tlgcohort.com>
2021-06-30 20:22:20 +0000

    studying for logic

---
+++ introfor/forloopc.py
@@ -0,0 +1,11 @@
+#define the list
+
+farms = [{"Doit": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"Ranch": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"Almost Vegan": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+#create for loop
+
+for name in farms:
+    print(name)
+
