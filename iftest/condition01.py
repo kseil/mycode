be762284cba745ff5f49057539c76e2091043b11
Keil <Keil.Schmit@tlgcohort.com>
2021-06-30 15:50:28 +0000

    if statements p1

---
+++ iftest/condition01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#create string hostname
+hostname = "MTG"
+
+#test with if statement, when found true
+
+if hostname == "MTG":
+    print("The hostname was found to be mtg.")
+
+
