8c4869e76b2ac4488393fd2ae92caf32c8a09d34
Keil <Keil.Schmit@tlgcohort.com>
2021-07-01 19:58:00 +0000

    Python and phhysical interfaces

---
+++ netfacd/interface_reader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import netifaces
+
+print(netifaces.interfaces())
+
+for i in netifaces.interfaces():
+    print('\n********Details of Interface - ' + i + ' ************')
+    try:
+        print('MAC: ', end='')
+        print((netifaces.ifaddresses(i)[netifaces.AF_LINK])[0]['addr']) # Prints the MAC address
+        print('IP: ', end='')
+        print(( netifaces.ifaddresses(i)[netifaces.AF_INET])[0]['addr']) # Prints the IP address
+    except:
+        print('Could not collect adapter information')
+
