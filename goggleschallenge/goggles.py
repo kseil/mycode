86e830850ff5a46e00320c912847148903469e0d
Keil <Keil.Schmit@tlgcohort.com>
2021-07-06 16:53:20 +0000

    goggleschallenge

---
+++ goggleschallenge/goggles.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+output1 ="My "+ challenge[2][1] + "! The "+ challenge[2][0]+ " do "+ challenge[3] + "!"
+print(output1)
+
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+output2 = "My "+ trial[2]["goggles"] + "! The "+ trial[2]["eyes"] + " do "+ trial[-1] + "!"
+print(output2)
+
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+#print(nightmare[0]['kumquat'])
+output3 = "My "+ nightmare[0]['user']['name']['first'] + "! The "+ nightmare[0]['kumquat'] + " do "+ nightmare[0]['d'] + "!"
+print(output3)
